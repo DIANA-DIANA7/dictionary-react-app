03c35f4f05a22d8cd96970c5ad7a1118f2d85af3
DIANA-DIANA7 <dianasmejia21@gmail.com>
2022-06-03 08:56:08 -0700

    Update Meaning.js
    map through multiple definitions displaying in div with example if provided.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 export default function Meaning(props){
-    console.log(props.meaning)
-    return "hello from meaning"
+    
+    return <div className="meanings"> 
+
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function(definition,index){
+            return (
+                <div key={index}>
+                    <p>
+                        {definition.definition}
+                        <br />
+                        <em>{definition.example}</em>
+                    </p>
+                </div>
+            )
+        })}
+    </div>
 }
