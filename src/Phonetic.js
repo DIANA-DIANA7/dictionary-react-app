f877be62ee61acf82a378a4a3ea915aff6866668
DIANA-DIANA7 <dianasmejia21@gmail.com>
2022-06-07 11:40:48 -0700

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props){
     
     return (
         <div className="phonetic">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noopener">
                 Listen
             </a>
             <br />
